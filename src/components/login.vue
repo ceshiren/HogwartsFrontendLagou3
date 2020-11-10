7fa18983f13c66924228acbaa1401dae8364cbfc
seveniruby <seveniruby@gmail.com>
2020-11-10 21:59:42 +0800

    fix layout center

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<v-main>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm8 md4>
@@ -38,7 +38,7 @@
           </v-flex>
         </v-layout>
       </v-container>
-</div>
+</v-main>
 </template>
 
 <script>
